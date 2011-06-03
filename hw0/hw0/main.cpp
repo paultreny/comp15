ffffe6ff077b2b9724aade24342ffb7b7da31721
Paul R <paultreny@gmail.com>
2011-06-03 11:31:35 -0400

    workspacedata?

---
+++ hw0/hw0/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  hw0
+//
+//  Created by Paul Reny on 6/3/11.
+//  Copyright 2011 Reny Design. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
