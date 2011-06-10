51168daf5de569e21f3f5e1498d42b4810ce5638
Paul R <paultreny@gmail.com>
2011-06-10 03:28:23 -0400

    a

--- hw0/hw0.xcodeproj/main.cpp
+++ hw0/hw0.xcodeproj/main.cpp
@@ -18,26 +18,27 @@ using namespace renyp_ns_zero;
 int main() { 
   
 	Bag bag_1;
-  element_t input = 0;
-  cout << "[hw0] Let's test some bags!" << endl;
-  cout << "[hw0] Prevented crash from removing from empty bag? ";
-  cout << boolalpha;
-  cout << !bag_1.remove() << endl;
-  cout << "[hw0] Enter numbers (separated by spaces)" << endl;
-  cout << "[hw0] Enter a negative number and press Enter when done:";
-  while (true)
+  element_t input = element_t();
+  cout << "[hw0] Let's test my Bag!" << endl;
+  cout << "[hw0] (T|F) Prevented crash from removing from empty bag? "; 
+  cout << boolalpha; // sets cout to display true/false instead of 1/0
+  cout << !bag_1.remove() << endl; // Bag.remove() returns 0 if empty
+  cout << "[hw0] Enter numbers (separated by spaces), then enter" << endl;
+  cout << "[hw0] a negative number and press Enter when done:";
+  while (true) // loop in as many numbers as user likes
   {
     cin >> input;
     if (input < 0) break;
-    bag_1.insert(input);
+    bag_1.insert(input); // test of Bag.insert(input)
   }
-  cout << "[hw0] Numbers inserted into Bag!" << endl;
-  cout << "[hw0] " << bag_1.size() << " items" << endl;
-  
-  for (pos_int i = bag_1.size(); i > 0; i--)
+  cout << "[hw0] " << bag_1.size() << " items inserted into Bag!" << endl;
+  cout << "[hw0] Checking if Bag is empty... " << bag_1.is_empty() << endl;
+  cout << "[hw0] ";
+  for (pos_int i = bag_1.size(); i > 0; i--) // removing items until empty
     {
-      cout << "[hw0] "<< bag_1.remove() << " removed." << endl;
+      cout << bag_1.remove() << " ";
     }
+  cout << "removed." << endl;
   cout << "[hw0] Checking if Bag is empty... " << bag_1.is_empty() << endl;
   
   cout << "[hw0] Goodbye!" << endl;
