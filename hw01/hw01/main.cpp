d76b4c2e1e059b96743ea6eb17a09b349c5cfd97
Paul R <paultreny@gmail.com>
2011-06-21 21:21:04 -0400

    initial commit

---
+++ hw01/hw01/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  hw01
+//
+//  Created by Paul Reny on 6/21/11.
+//  Copyright 2011 Reny Design. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
