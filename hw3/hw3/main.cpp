095ba1bf605a88595220482d0f304444da831a1a
Paul Reny <paultreny@gmail.com>
2011-07-25 14:53:41 -0400

    changed main() in stopwords.cpp to stopword(), was causing conflict

--- hw3/hw3/main.cpp
+++ hw3/hw3/main.cpp
@@ -4,6 +4,12 @@
 //
 //  Copyright 2011 Reny Design. All rights reserved.
 //
+//
+//
+// Your job is to choose one of the data structures covered in class that can map a character string to a integer, with the objective of optimizing retrieval time. You are expected to justify your decision.
+//
+//
+//
 
 #include <iostream>
 #include <cstdlib>
@@ -15,14 +21,34 @@ int main (int argc, const char * argv[])
 {
   cout << "HW3" << endl;
   
+  // CREATE DATA STRUCTURE
+  
   while (!cin.eof())
     {
+      // CHECK IF NOT A LETTER (isalpha)
+    
+      // CONVERT TO LOWERCASE (tolower)
+    
+      // READ WORD
       
+      // COMPARE TO STOP WORDS TRIE
       
+      // POTENTIALLY USE STEMMER, TO MAKE WORDS MORE SIMPLE? //
+      
+      // ADD WORD TO DATA STRUCTURE
+      // & INCREMENT COUNT OF WORD
       
       
     }
   
+  // DATA STRUCTURE THOUGHTS
+  //
+  // Heap/Priority Queue might be good, would keep track of words with the highest occurance
+  //
+  //
+  //
+  //
+  // 
   
   
   
