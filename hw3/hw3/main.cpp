55ec082267a6b0c0305decba88756b73cac244be
Paul Reny <paultreny@gmail.com>
2011-07-25 19:12:33 -0400

    starting to work!

--- hw3/hw3/main.cpp
+++ hw3/hw3/main.cpp
@@ -53,25 +53,34 @@ int main (int argc, const char * argv[])
 //  stopword_trie stop_words;
   const unsigned int TBLSZ = 2000003; // choose a prime just in case
                                       // other fine choices:  1000003 2584853 3072263 8004823
-  const char FILENAME[] = "/usr/share/dict/words";
+  const char STOPFILE[] = "/Users/paulreny/GitHub/comp15/hw3/hw3/stopwords.txt"; // /usr/share/dict/words
   unsigned int keys = 0;			// 	and keys inserted into table
 	unsigned int *table = new unsigned int[TBLSZ]; 
-	for (unsigned int i=0; i<TBLSZ; i++) { table[i] = 0; }
-	for (ifstream fin(FILENAME); !fin.eof(); fin>>ws) {
-		if (!fin) { cerr << "Error:  Failed to read from input file \"" << FILENAME << "\"" << endl; exit(-1); }
+	
+  for (unsigned int i=0; i<TBLSZ; i++)
+  {
+    table[i] = 0; 
+  }
+  
+	for (ifstream fin(STOPFILE); !fin.eof(); fin>>ws)
+  {
+		if (!fin) { cerr << "Error:  Failed to read from input file \""
+                     << STOPFILE << "\"" << endl; exit(-1); }
     string w;
 		fin >> w;
     
 		unsigned long hv = hash(w); 
-		if (hv != hash(w)) { cerr << "The hash function is BROKEN." << endl; exit(-1); }
-    
-		if (table[hv % TBLSZ]) { collisions++; }
 		
+    if (hv != hash(w)) { cerr << "The hash function is BROKEN." << endl; exit(-1); }
+    
+		if ( table[hv % TBLSZ] )
+    {
+      cout << "Stop Word!" << endl;
+    }
 		table[hv % TBLSZ]++;
 		keys++;
-    cout << collisions << " collisions / " << keys << " keys = " << (100.0 * collisions / keys) << "%" << endl;
-
 	}
+  /*
   const string STOPWORDS[] = { "a", "about", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "amoungst", "amount", "an", "and", "another", "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are", "around", "as", "at", "back", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom", "but", "by", "call", "can", "cannot", "cant", "co", "computer", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "i", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own", "part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thick", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "" };
 
 	for (int i=0; STOPWORDS[i] != ""; i++ )
@@ -80,17 +89,37 @@ int main (int argc, const char * argv[])
 //		stop_words.add(STOPWORDS[i], STOPWORDS[i].size(), i);
 	}
   
-  
+  */
+  cout << "Stop words hashed: " << keys << endl;
   cout << "HW3" << endl;
   
-  
+  const char FILENAME[] = "/Users/paulreny/GitHub/comp15/hw3/hw3/bard.txt";
   // CREATE DATA STRUCTURE
-  return EXIT_SUCCESS;
-  
-
-  
-  //while (!cin.eof())
-    {
+  int count = 0;
+  for (ifstream fin(FILENAME); !fin.eof(); fin>>ws)
+ // while (!cin.eof())
+  {
+      string word;
+      bool isWord = true;
+      fin >> word;
+      
+      for (int i=0; i<word.size(); ++i )
+      {
+        isWord = isalpha(word[i]);
+        if (!isWord)
+        {
+          break;
+        }
+        word[i] = tolower(word[i]);
+      }
+      if (isWord && !table[hash(word) % TBLSZ])
+      {
+        cout << word << endl;
+        count++;
+      }
+  }
+  cout << count << endl;
+      
       // CHECK IF NOT A LETTER (isalpha)
     
       // CONVERT TO LOWERCASE (tolower)
@@ -104,8 +133,6 @@ int main (int argc, const char * argv[])
       // ADD WORD TO DATA STRUCTURE
       // & INCREMENT COUNT OF WORD
       
-      
-    }
   
   // DATA STRUCTURE THOUGHTS
   //
@@ -116,12 +143,7 @@ int main (int argc, const char * argv[])
   //
   // 
   
-  
-  
-  
-  
-  
-  
+
   
   return EXIT_SUCCESS;
 }
