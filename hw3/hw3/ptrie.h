71daf1983e754134008ba4c2078ac5b7671e251e
Paul Reny <paultreny@gmail.com>
2011-07-25 16:55:24 -0400

    working on ptrie.h

--- hw3/hw3/ptrie.h
+++ hw3/hw3/ptrie.h
@@ -7,3 +7,48 @@
 //
 //
 
+#ifndef PTRIE_H
+#define PTRIE_H
+
+#include <string>
+
+using namespace std;
+
+template <typename VALUE, int RANGE, typename KEYTYPE>
+struct ptrie
+{
+  typedef VALUE value_t;
+  enum
+  {
+    range_mask = RANGE-1,
+    range = RANGE
+  };
+  typedef KEYTYPE key_t;
+  typedef ptrie<VALUE,RANGE,KEYTYPE> type_t;
+  
+  value_t result;
+  type_t * parent;
+  type_t * table[RANGE];
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+private:
+  void next();
+  
+  
+  
+};
+
+#endif
+
