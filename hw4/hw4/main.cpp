3afbde285d7d39db579e691a2754c4229a7206bb
Paul Reny <paultreny@gmail.com>
2011-08-09 02:50:44 -0400

    Reny Design

--- hw4/hw4/main.cpp
+++ hw4/hw4/main.cpp
@@ -3,7 +3,7 @@
 //  hw4
 //
 //  Created by Paul Reny on 8/9/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Reny Design. All rights reserved.
 //
 
 #include <iostream>
